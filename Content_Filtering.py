3b170211031e68897584523a116079885da719c4
Alchemist <rhithick02@gmail.com>
2020-12-25 16:33:02 +0530

    Content and CF

---
+++ Content_Filtering.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+dataframe = pd.read_csv("movie_dataset.csv")
+print(dataframe)
+features = ['keywords', 'cast', 'genres', 'director']
+for feature in features:
+    dataframe[feature] = dataframe[feature].fillna('')
+
+def combine_features(row):
+    combine = ""
+    for i in features:
+        combine += str(row[i])
+        combine += " "
+    return combine
+
+dataframe["combined_features"] = dataframe.apply(combine_features, axis = 1)
+
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(dataframe["combined_features"])
+similarity = cosine_similarity(count_matrix)
+
+def get_title_from_index(index):
+	return dataframe[dataframe.index == index]["title"].values[0]
+
+def get_index_from_title(title):
+	return dataframe[dataframe.title == title]["index"].values[0]
+
+movie_user_likes = "Guardians of the Galaxy"
+movie_index = get_index_from_title(movie_user_likes)
+similar_movies = list(enumerate(similarity[movie_index]))
+sorted_sim_movies = sorted(similar_movies, key = lambda x:x[1], reverse = True)
+
+cnt = 0
+for movie in sorted_sim_movies:
+    print(get_title_from_index(movie[0]))
+    cnt = cnt + 1
+    if cnt > 20: 
+        break
