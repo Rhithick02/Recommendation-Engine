6a8a3a300fd5cd6da0d498af7bbc93f5d110a9f9
Alchemist <rhithick02@gmail.com>
2021-01-02 23:48:33 +0530

    Added background image for search bar

--- New_Content_Filtering/djangonautic/djangonautic/settings.py
+++ New_Content_Filtering/djangonautic/djangonautic/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+tloc = os.path.join(BASE_DIR, 'templates/')
+sloc = os.path.join(BASE_DIR, 'static/')
+# print(loc)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -37,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mengine',
 ]
 
 MIDDLEWARE = [
+    # 'megine.apps.MengineConfig',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'djangonautic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [tloc],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    sloc,
+]
--- New_Content_Filtering/djangonautic/djangonautic/urls.py
+++ New_Content_Filtering/djangonautic/djangonautic/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, re_path
-from . import views
+from django.urls import path, re_path, include
+# from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('^$', views.homepage),
-    re_path('about/', views.about),
+    path('', include('mengine.urls'))
+    # re_path('^$', views.homepage),
+    # re_path('about/', views.about),
 ]
